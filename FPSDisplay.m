d393d8e10975d97b77af193074854ce5e78105e1
Khổng Mạnh Yên <yenvoiu@gmail.com>
2025-10-27 16:53:06 +0700

    Update FPSDisplay.m

--- FPSDisplay.m
+++ FPSDisplay.m
@@ -79,38 +79,32 @@ CGRect frame = CGRectMake([UIScreen mainScreen].bounds.size.width / 4 - 4, [UISc
     }
 }
 
-- (void)updateDisplayLabelText: (float) fps
+- (void)updateDisplayLabelText:(float)fps
 {
+    // Cập nhật chữ
+    self.displayLabel.text = @"Khổng Mạnh Yên";
 
-
-
-    NSMutableString *mustr = [[NSMutableString alloc] init];
-    [mustr appendFormat:@"%@",self.getSystemDate];
-
-
-    self.displayLabel.text = [NSString stringWithFormat:@"Khổng Mạnh Yên"];//fps
-
-        // Tạo màu rainbow dựa theo thời gian
+    // Hiệu ứng đổi màu cầu vồng
     static CGFloat hue = 0;
-    hue += 0.002; // tốc độ đổi màu
+    hue += 0.03; // tốc độ đổi màu (có thể tăng lên nếu muốn nhanh hơn)
     if (hue > 1.0) hue -= 1.0;
-
     UIColor *rainbowColor = [UIColor colorWithHue:hue saturation:1.0 brightness:1.0 alpha:1.0];
     self.displayLabel.textColor = rainbowColor;
 
-    self.displayLabel.font = [UIFont boldSystemFontOfSize:8];
+    // Font chữ to, đậm, bo tròn nhẹ
+    self.displayLabel.font = [UIFont fontWithName:@"AvenirNext-Bold" size:18];
+    self.displayLabel.layer.cornerRadius = 8;
+    self.displayLabel.clipsToBounds = YES;
+    self.displayLabel.textAlignment = NSTextAlignmentCenter;
+
+    // Hiệu ứng glow (phát sáng xung quanh)
+    self.displayLabel.layer.shadowColor = rainbowColor.CGColor;
+    self.displayLabel.layer.shadowRadius = 6.0;
+    self.displayLabel.layer.shadowOpacity = 0.9;
+    self.displayLabel.layer.shadowOffset = CGSizeZero;
+
+    // Thêm nền mờ nhẹ phía sau cho nổi bật
+    self.displayLabel.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.25];
 }
-
-- (NSString *)getSystemDate
-{
-    NSDate *currentDate = [NSDate date];
-    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-
-
-    [dateFormatter setDateFormat:@"EEEE,dd/MM/yyyy | HH:mm:ss"];
-
-    return [dateFormatter stringFromDate:currentDate];
-}
-
 @end
 
